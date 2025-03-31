fdc693645f20e573fb2b35afe369dd07fd6f6d57
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2025-03-31 16:10:57 +0700

    display popular courses, edit view user library

--- DTOs/Course/PopularCourseDTO.cs
+++ DTOs/Course/PopularCourseDTO.cs
@@ -2,14 +2,13 @@
 
 namespace QuizletClone.DTOs.Course
 {
-    public class RecentCourseDTO
+    public class PopularCourseDTO
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Slug { get; set; } = string.Empty;
         public UserDTO OwnerDTO { get; set; } = new UserDTO();
         public string? Password { get; set; } = string.Empty;
-        public string RelativeTime { get; set; } = string.Empty;
         public int FlashcardCount { get; set; }
     }
 }
--- Mappers/CourseMapper.cs
+++ Mappers/CourseMapper.cs
@@ -94,13 +94,25 @@ namespace QuizletClone.Mappers
             return new RecentCourseDTO
             {
                 Id = courseProgress.Course!.Id,
-                OwnerUserId = courseProgress.Course.UserId,
-                OwnerUsername = courseProgress.Course.User?.UserName ?? string.Empty,
+                OwnerDTO = courseProgress.Course.User!.ToUserDTO(),
                 Title = courseProgress.Course.Title,
                 Password = courseProgress.Course.Password,
                 Slug = courseProgress.Course.Slug,
                 RelativeTime = courseProgress.Course.CreatedAt.ToRelativeTime(),
                 FlashcardCount = courseProgress.Course.Flashcards.Count
+            };
+        }
+
+        public static PopularCourseDTO ToPopularCourseDTO(this Course course)
+        {
+            return new PopularCourseDTO
+            {
+                Id = course!.Id,
+                OwnerDTO = course.User!.ToUserDTO(),
+                Title = course.Title,
+                Password = course.Password,
+                Slug = course.Slug,
+                FlashcardCount = course.Flashcards.Count
             };
         }
 
